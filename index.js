52b32185ec0d6ab542ece896b65400706d6acc0e
unknown <lenny>
2022-11-30 15:57:28 +0300

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+// MULTIPLY FUNCTION
+const multiply = (num1, num2) => {
+    return num1 * num2;
+  };
+
+  let resulMultiply = multiply(31, 2);
+  console.log(resulMultiply);
+  // 62
